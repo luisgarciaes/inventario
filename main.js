a8857532e6f997834e34aaa661eaade1c0f60686
ruruisu <zaddozgl@gmail.com>
2021-09-19 20:52:36 -0500

    activity history

--- main.js
+++ main.js
@@ -4,14 +4,34 @@ import Registry from "./registry.js";
 class App {
     constructor(){
     this._registry = new Registry()
+    
+    this._consoleText = document.querySelector("#console")
+
     let btnAdd = document.querySelector("#btnAdd");
     btnAdd.addEventListener("click", this._addProduct);
-    let btnShow = document.querySelector("#btnRemove");
-    btnShow.addEventListener("click", this._showArr);
+    let btnAddPosition = document.querySelector("#btnAddPosition");
+    btnAddPosition.addEventListener("click", this._showArr);
+    let btnRemove = document.querySelector("#btnRemove");
+    btnRemove.addEventListener("click", this._showArr)
+    let btnSearch = document.querySelector("#btnSearch");
+    btnSearch.addEventListener("click", this._showArr);
+    let btnReturn = document.querySelector("#btnReturn");
+    btnReturn.addEventListener("click", this._showArr);
+    let btnReturnReverse = document.querySelector("#btnReturnReverse");
+    btnReturnReverse.addEventListener("click", this._showArr);
     }
-    _showArr = () =>{
-        console.log(this._registry)
-        return this._registry
+
+    _updateConsole = (x,y) =>{
+        let message = "";
+        switch (y) {
+            case 1:
+                message = "Se agrego el producto " + x + " correctamente.";
+                break;
+            case 2:
+                message = "Se borró el producto " + x + " correctamente.";
+                break;
+        }
+        document.querySelector("#console").innerHTML = message;
     }
     _addProduct = () => {
         let product = Product.readForm()
@@ -24,7 +44,11 @@ class App {
             Swal.fire("Error", "Producto ya registrado", "error");
             return;
         }
+        this._updateConsole(product._name,1)
         Swal.fire("Correcto", "Se agregó un nuevo producto", "success");
+    };
+
+    _removeProduct = () => {
     };
 }
 new App;
